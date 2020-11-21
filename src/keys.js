7e55b101affe3ccd4532326f435b011e42864dbe
Guillermoç <guillermodean@hotmail.com>
2020-11-21 18:06:10 +0100

    actualizado para herokuDB

--- src/keys.js
+++ src/keys.js
@@ -1,11 +1,11 @@
 module.exports= {
 
  database: {
-    host:'localhost',
-    user: 'root',
-    password: 'Guillermo11',
-    database: 'megalitos'
+    host:'eu-cdbr-west-03.cleardb.net',
+    user: 'bf82d1ee94f57f',
+    password: '48fcd9dc',
+    database: 'heroku_b5b792ba7e6720'
 
  }
-
+//mysql://bf82d1ee94f57f:48fcd9dc@eu-cdbr-west-03.cleardb.net/heroku_b5b792ba7e6720d?reconnect=true
 }
