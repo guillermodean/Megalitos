5c1d3454ba4a03c459f02a65d051b5f2bddfb05b
Guillermoç <guillermodean@hotmail.com>
2020-11-23 19:54:08 +0100

    html css correcciones cambio paleta

--- src/keys.js
+++ src/keys.js
@@ -8,17 +8,10 @@ module.exports= {
     password: 'ya59s9n0iyv9wrbd',
     database: 'tjtw081bf1rlbbxm'}
 
- //database: {
- //   host:'eu-cdbr-west-03.cleardb.net',
- //   user: 'bf82d1ee94f57f',
- //   password: '48fcd9dc',
- //   database: 'heroku_b5b792ba7e6720'}
  //database:{
  //   host:'localhost',
  //   user:'root',
  //   password:'Guillermo11',
  //   database: 'megalitos'}
- 
- 
-//mysql://bf82d1ee94f57f:48fcd9dc@eu-cdbr-west-03.cleardb.net/heroku_b5b792ba7e6720d?reconnect=true
+
 }
