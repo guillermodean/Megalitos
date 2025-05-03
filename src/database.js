a47fa7011608505cac4991fac55e21b6b58e1624
Guillermoç <guillermodean@hotmail.com>
2025-05-03 10:04:57 +0200

    updated connection

--- src/database.js
+++ src/database.js
@@ -1,26 +1,36 @@
-const mysql= require('mysql');
-const {database } = require('./keys');
-const pool = mysql.createPool(database);
-const {promisify}= require('util')
+const mysql = require('mysql2');
+const { database } = require('./keys');
 
-pool.getConnection((err,connection) =>{
-if (err) {
-    if (err.code=== 'PROTOCOL_CONNECTION_LOST'){
-        console.error('database connection was closed');
-           }
-    if (err.code === 'ER_CON_COUNT_ERROR'){
-        console.error('too many connections');
-    }
-    if (err.code ==='ECONNREFUSED'){
-        console.error('database conection was refused')
-    }
-}
-if (connection) {
-    connection.release();
-    console.log('db is conected');}
-
-return;
+const pool = mysql.createPool({
+  ...database,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  ssl: { rejectUnauthorized: true } // Recomendado para Railway
 });
-//promisify pool querys lo que antes eran callbacks
-pool.query=promisify(pool.query);
+
+// Promisify queries
+const { promisify } = require('util');
+pool.query = promisify(pool.query);
+
+// Prueba de conexión
+pool.getConnection((err, connection) => {
+  if (err) {
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      console.error('Database connection was closed.');
+    }
+    if (err.code === 'ER_CON_COUNT_ERROR') {
+      console.error('Database has too many connections.');
+    }
+    if (err.code === 'ECONNREFUSED') {
+      console.error('Database connection was refused.');
+    }
+    return;
+  }
+  if (connection) {
+    connection.release();
+    console.log('DB is connected');
+  }
+});
+
 module.exports = pool;
--- src/keys.js
+++ src/keys.js
@@ -1,17 +1,19 @@
 
 module.exports= {
 
- database: {
-    host:'database-1.cev9ngb4mxsp.us-east-1.rds.amazonaws.com',
-    user: 'admin',
-    password: 'Sf$wfb8ezQdvLia9',
-    database: 'megalitos',
-    connectTimeout: 60000}
+//  database: {
+//     host:'database-1.cev9ngb4mxsp.us-east-1.rds.amazonaws.com',
+//     user: 'admin',
+//     password: 'Sf$wfb8ezQdvLia9',
+//     database: 'megalitos',
+//     connectTimeout: 60000}
 
- //database:{
- //   host:'localhost',
- //   user:'root',
- //   password:'Guillermo11',
- //   database: 'megalitos'}
+ database:{
+   host:'tramway.proxy.rlwy.net',
+   port:28635,
+   user:'root',
+   password:'kzdusxWyyWBsvSEYRSIDfaKKHlgHoUst',
+   database: 'railway'},
+   ssl: { rejectUnauthorized: true }
 
 }
